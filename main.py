6c92dfef0def150433057f688c1da3ffa01298c4
Supriya Pandurangacharya Upadhyaya <upadhyaya.supriya@gmail.com>
2024-05-04 20:19:44 +0200

    Import torch

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ st.title("Langchain NL2SQL Chatbot")
 
 from unsloth import FastLanguageModel
 from transformers import LlamaTokenizer, LlamaForCausalLM, AutoTokenizer
+import torch
 
 
 max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
